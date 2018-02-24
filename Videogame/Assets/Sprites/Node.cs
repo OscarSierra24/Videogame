16d4608d981b83de2f8306e2186d69df7f2aea86
Daniela González Güémez <danielaguemez@gmail.com>
2018-02-24 16:11:51 -0600

    dani

---
+++ Videogame/Assets/Sprites/Node.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour {
+    public Node[] children;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnDrawGizmos()
+    {
+
+        // sirve para dibujar Gizmos
+        // gizmos - gráficos de ayuda a desarrollador
+        /*Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, 1);
+
+        if (children != null && children.Length > 0)
+        {
+
+            Gizmos.color = Color.green;
+            for (int i = 0; i < children.Length; i++)
+            {
+
+                Gizmos.DrawLine(
+                    transform.position,
+                    children[i].transform.position
+                );
+            }
+        }*/
+    }
+}
---
+++ Videogame/Assets/Sprites/goomba.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class goomba : MonoBehaviour {
+
+    public Node[] path;
+    public float threshold;
+    private Node currNode;
+    private int currNodeIndex;
+    private Animator a;
+
+    // Use this for initialization
+    void Start () {
+        a = GetComponent<Animator>();
+        currNodeIndex = 0;
+        currNode = path[currNodeIndex];
+    }
+
+    // Update is called once per frame
+    void Update() {
+        //transform.Translate(Vector3.right * Time.deltaTime*.7f);
+        if (Vector3.Distance(transform.position, currNode.transform.position) < threshold) {
+            if (currNodeIndex + 1 < path.Length)
+            {
+                currNodeIndex++;
+                a.SetBool("direction", false);
+
+            }
+            else {
+                currNodeIndex = 0;
+                a.SetBool("direction", true);
+            }
+            currNode = path[currNodeIndex];
+        }
+
+        /*Vector3 patrolPointDir = currNode.transform.position - transform.position;
+        float angle = Mathf.Atan2(patrolPointDir.x, patrolPointDir.y) * Mathf.Rad2Deg-90f;
+
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.up);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, q, 90f);*/
+        transform.position = Vector2.MoveTowards(new Vector2(transform.position.x, transform.position.y), currNode.transform.position, 0.7f * Time.deltaTime);
+
+    }
+
+}
